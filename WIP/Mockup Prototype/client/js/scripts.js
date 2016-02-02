a0176595143419a133c92f009b668107b139b21b
Nguyễn Hải Đăng <haidang.cva@gmail.com>
2016-02-02 13:38:10 +0700

    update mockup prototype
    update mockup prototype
    note: home page change everything

--- WIP/Mockup Prototype/client/js/scripts.js
+++ WIP/Mockup Prototype/client/js/scripts.js
@@ -1,26 +1,9 @@
-
+<!-- begin register-->
 jQuery(document).ready(function() {
 
-    /*
-        Background slideshow
-    */
-    // $.backstretch([
-    //   "assets/img/backgrounds/1.jpg"
-    // , "assets/img/backgrounds/2.jpg"
-    // , "assets/img/backgrounds/3.jpg"
-    // ], {duration: 3000, fade: 750});
-
-    /*
-        Tooltips
-    */
-    // $('.links a.home').tooltip();
-    // $('.links a.blog').tooltip();
-
-    /*
-        Form validation
-    */
     $('.register form').submit(function(){
         $(this).find("label[for='firstname']").html('Tên');
+        $(this).find("label[for='lastname']").html('Họ');
         $(this).find("label[for='confirm']").html('Xác nhận mật khẩu');
         $(this).find("label[for='username']").html('Tài khoản');
         $(this).find("label[for='email']").html('Email');
@@ -31,16 +14,17 @@ jQuery(document).ready(function() {
         var username = $(this).find('input#username').val();
         var email = $(this).find('input#email').val();
         var password = $(this).find('input#password').val();
+        if(lastname == '') {
+            $(this).find("label[for='lastname']").append("<span style='display:none' class='red'> - Please enter your last name.</span>");
+            $(this).find("label[for='lastname'] span").fadeIn('medium');
+            return false;
+        }
         if(firstname == '') {
             $(this).find("label[for='firstname']").append("<span style='display:none' class='red'> - Please enter your first name.</span>");
             $(this).find("label[for='firstname'] span").fadeIn('medium');
             return false;
         }
-        if(confirm == '') {
-            $(this).find("label[for='confirm']").append("<span style='display:none' class='red'> - Please enter your last name.</span>");
-            $(this).find("label[for='confirm'] span").fadeIn('medium');
-            return false;
-        }
+
         if(username == '') {
             $(this).find("label[for='username']").append("<span style='display:none' class='red'> - Please enter a valid username.</span>");
             $(this).find("label[for='username'] span").fadeIn('medium');
@@ -56,9 +40,68 @@ jQuery(document).ready(function() {
             $(this).find("label[for='password'] span").fadeIn('medium');
             return false;
         }
+        if(confirm == '') {
+            $(this).find("label[for='confirm']").append("<span style='display:none' class='red'> - Password doesn't match.</span>");
+            $(this).find("label[for='confirm'] span").fadeIn('medium');
+            return false;
+        }
     });
 
 
 });
+<!-- end register-->
 
+<!--begin: edit medicinal plant and remedy article-->
+$(document).ready(function(){
+    $("#edit").click(function(){
+      $("#note").attr("readonly", false);
+      $("#note1").attr("readonly", false);
+      $("#note2").attr("readonly", false);
+      $("#note3").attr("readonly", false);
+      $("#note4").attr("readonly", false);
+      $("#note5").attr("readonly", false);
+      $("#note6").attr("readonly", false);
+    });
+    $("#save").click(function(){
+      $("#note").attr("readonly", true);
+      $("#note1").attr("readonly", true);
+      $("#note2").attr("readonly", true);
+      $("#note3").attr("readonly", true);
+      $("#note4").attr("readonly", true);
+      $("#note5").attr("readonly", true);
+      $("#note6").attr("readonly", true);
+    });
+});
+<!--end: edit medicinal plant and remedy article-->
 
+<!-- begin: searchable-->
+$(document).ready(function () {
+    (function ($) {
+
+        $('#filter').keyup(function () {
+            var rex = new RegExp($(this).val(), 'i');
+            $('.searchable .searchable-item').hide();
+            $('.searchable .searchable-item').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+
+        })
+    }(jQuery));
+});
+<!-- end: searchable-->
+<!-- begin: search herb medicinal plant, remedy -->
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+<!-- end: search herb medicinal plant, remedy-->
